bdbb9da41af37821a0a99308a888908d4d25b326
Aayushtamang <jimba.lama22@gmail.com>
2026-07-28 16:29:15 +0545

    food app

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,7 +1,7 @@
-import logo from './logo.png'
+import logo from './restaurant.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
+import profile_image from './account.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
