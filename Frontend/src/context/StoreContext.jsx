5bf08ee1876aed39715586a50753d78f0c7556ce
Aayushtamang <jimba.lama22@gmail.com>
2026-08-08 18:43:26 +0545

    footer component created

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -1,13 +1,38 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
+    const [cartItems, setCartItems] = useState({});
+
+    // If user is adding the item for the first time
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]) {
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        } 
+        // Adding item in existing item
+        else {
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+    },[cartItems])
+
 
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
 
     
