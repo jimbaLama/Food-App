f069581d5ece884d24d2396e265d77de2e57c6cb
Aayushtamang <jimba.lama22@gmail.com>
2026-08-17 21:34:08 +0545

    myorder tracking page created

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -102,4 +102,38 @@ const userOrders = async (req, res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrders}
+// List order for admin panel
+const listOrders = async (req, res) => {
+    try {
+        const orders = await oredrModel.find({});
+        res.json({
+            success: true,
+            data: orders
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: "Error!"
+        })
+    }
+}
+
+// api for updating order status
+const updateStatus = async (req, res) => {
+    try {
+        await oredrModel.findByIdAndUpdate(req.body.orderId, {status:req.body.status});
+        res.json({
+            success: true,
+            message: "Status Updated"
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: "Error!"
+        })
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus}
