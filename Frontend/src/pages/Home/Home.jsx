9edb5ee3e8db04677a0040684ca3728f965097aa
Aayushtamang <jimba.lama22@gmail.com>
2026-09-15 21:03:17 +0545

    fix foodDisplay error

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,24 +1,39 @@
-import { useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
-import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import Header from '../../components/Header/Header'
-import './Home.css'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-// import AppDownload from '../../components/AppDownload/AppDownload'
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import Header from "../../components/Header/Header";
+import "./Home.css";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 
 const Home = () => {
-
   const [category, setCategory] = useState("All");
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const searchTerm = searchParams.get("search") || "";
+
+  const handleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+
+    // Clear the previous search
+    searchParams.delete("search");
+    setSearchParams(searchParams);
+  };
 
   return (
     <div>
       <Header />
-      <ExploreMenu category={category} setCategory={setCategory} />
-      <FoodDisplay category={category} searchTerm={searchTerm} />
-    </div>
-  )
-}
 
-export default Home
+      <ExploreMenu
+        category={category}
+        setCategory={handleCategoryChange}
+      />
+
+      <FoodDisplay
+        category={category}
+        searchTerm={searchTerm}
+      />
+    </div>
+  );
+};
+
+export default Home;
